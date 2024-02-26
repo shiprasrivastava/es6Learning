1f09881f02f08f50a715efd7c0783f25c0c4ac28
shiprasrivastava <ship0008@gmail.com>
2024-02-26 10:58:08 +0530

    update

--- practice.js
+++ practice.js
@@ -1,3 +1,24 @@
+// const
 const pi = 3.14;
 let r=2;
 console.log(pi*r*r);
+
+//var and let
+
+var x = 10;
+{
+  let x = 2;
+  console.log(x);
+}
+console.log(x);
+
+//function
+var y = 20;
+const z = (x, y) => x * y;
+console.log(z);
+
+const fruits = ["apple","banana","pear"];
+for (let fruit of fruits) {
+  
+  console.log(fruit);
+};
